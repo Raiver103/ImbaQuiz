a0c6aeb2d28a99e5788f6523426096f3a348695c
raiver <ilya.maksimovich.01@gmail.com>
2025-03-27 12:01:19 +0300

    Initial commit

---
+++ ImbaQuiz.Domain/Entities/Quiz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImbaQuiz.Domain.Entities
+{
+    internal class Quiz
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+    }
+}
