86cf69ff3722e04099479baca1f12b48581e6b32
raiver <ilya.maksimovich.01@gmail.com>
2025-03-31 14:29:15 +0300

    add base entities

---
+++ ImbaQuiz.Domain/Entities/Answer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImbaQuiz.Domain.Entities
+{
+    public class Answer
+    { 
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool IsCorrect { get; set; }  
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
