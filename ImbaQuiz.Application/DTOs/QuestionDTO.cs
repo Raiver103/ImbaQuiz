4a9f6edcb4064b571227f5aeee8b30800545e3e3
raiver <ilya.maksimovich.01@gmail.com>
2025-04-01 10:40:47 +0300

    feature/add mapping

---
+++ ImbaQuiz.Application/DTOs/QuestionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImbaQuiz.Application.DTOs
+{
+    public class QuestionDTO
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int QuizId { get; set; }
+    }
+}
