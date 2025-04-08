ee364bec8344db6fb24c70140d656c98bb7f1d96
raiver <ilya.maksimovich.01@gmail.com>
2025-04-08 13:26:09 +0300

    add logging logic to controllers

--- quiz-service/ImbaQuiz.API/Controllers/AnswerController.cs
+++ quiz-service/ImbaQuiz.API/Controllers/AnswerController.cs
@@ -2,59 +2,74 @@
 using ImbaQuiz.Application.Interfaces;
 using ImbaQuiz.Domain.Entities;
 using ImbaQuiz.Domain.Exceptions;
+using ImbaQuiz.infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ImbaQuiz.API.Controllers
 { 
     [ApiController]
     [Route("api/answers")]
-    public class AnswersController(IAnswerService _answerService) : ControllerBase
+    public class AnswersController(IAnswerService _answerService, ILogSender _logSender) : ControllerBase
     { 
         [HttpGet]
         public async Task<OkObjectResult> GetAllAnswers(CancellationToken cancellationToken)
         {
+            _logSender.SendLog("Fetching all answers");
             var answers = await _answerService.GetAllAsync(cancellationToken);
+            _logSender.SendLog($"Fetched {answers.ToList().Count} answers");
             return Ok(answers);
         }
 
         [HttpGet("{id}")]
         public async Task<OkObjectResult> GetAnswer(int id, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Fetching answer with id {id}");
             var answer = await _answerService.GetByIdAsync(id, cancellationToken);
             if (answer is null) {
+                _logSender.SendLog($"Answer with id {id} not found.");
                 throw new NotFoundException($"Answer with id {id} not found.");
             }
+            _logSender.SendLog($"Answer with id {id} found.");
             return Ok(answer);
         }
 
         [HttpPost]
         public async Task<CreatedAtActionResult> CreateAnswer([FromBody] AnswerDTO answerDto, CancellationToken cancellationToken)
         {
+            _logSender.SendLog("Creating a new answer");
             var createdAnswer = await _answerService.CreateAsync(answerDto, cancellationToken);
+            _logSender.SendLog($"Created answer with id {createdAnswer.Id}");
             return CreatedAtAction(nameof(GetAnswer), new { id = createdAnswer.Id }, createdAnswer);
         }
 
         [HttpPut("{id}")]
         public async Task<NoContentResult> UpdateAnswer(int id, [FromBody] AnswerDTO answerDto, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Updating answer with id {id}");
             await _answerService.UpdateAsync(id, answerDto, cancellationToken);
+            _logSender.SendLog($"Updated answer with id {id}");
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<NoContentResult> DeleteAnswer(int id, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Deleting answer with id {id}");
             await _answerService.DeleteAsync(id, cancellationToken);
+            _logSender.SendLog($"Deleted answer with id {id}");
             return NoContent();
         }
 
         [HttpGet("by-question/{questionId}")]
         public async Task<OkObjectResult> GetAnswersByQuestionId(int questionId, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Fetching answers for question with id {questionId}");
             var answers = await _answerService.GetByQuestionIdAsync(questionId, cancellationToken);
             if (answers is null) {
-                throw new NotFoundException($"Answers are not found.");
+                _logSender.SendLog($"Answers for question with id {questionId} not found.");
+                throw new NotFoundException("Answers are not found.");
             }
+            _logSender.SendLog($"Fetched {answers.ToList().Count} answers for question with id {questionId}");
             return Ok(answers);
         }
     }
--- quiz-service/ImbaQuiz.API/Controllers/QuizController.cs
+++ quiz-service/ImbaQuiz.API/Controllers/QuizController.cs
@@ -1,7 +1,9 @@
-﻿using ImbaQuiz.Application.DTOs;
+﻿using ImbaQuiz.API.Services;
+using ImbaQuiz.Application.DTOs;
 using ImbaQuiz.Application.Interfaces;
 using ImbaQuiz.Domain.Entities;
 using ImbaQuiz.Domain.Exceptions;
+using ImbaQuiz.infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,45 +12,56 @@ namespace ImbaQuiz.API.Controllers
 { 
     [ApiController]
     [Route("api/quizzes")]
-    public class QuizzesController(IQuizService _quizService) : ControllerBase
-    { 
+    public class QuizzesController(IQuizService _quizService, ILogSender _logSender) : ControllerBase
+    {
 
         [HttpGet]
         public async Task<OkObjectResult> GetAllQuizzes(CancellationToken cancellationToken)
         {
+            _logSender.SendLog("Fetching all quizzes");
             var quizzes = await _quizService.GetAllAsync(cancellationToken);
+            _logSender.SendLog($"Fetched {quizzes.Count()} quizzes");
             return Ok(quizzes);
         }
 
         [HttpGet("{id}")]
         public async Task<OkObjectResult> GetQuiz(int id, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Fetching quiz with id {id}");
             var quiz = await _quizService.GetByIdAsync(id, cancellationToken);
-            if (quiz is null) 
-            { 
+            if (quiz is null)
+            {
+                _logSender.SendLog($"Quiz with id {id} not found.");
                 throw new NotFoundException($"Quiz with id {id} not found.");
             }
+            _logSender.SendLog($"Quiz with id {id} found.");
             return Ok(quiz);
         }
 
         [HttpPost]
         public async Task<CreatedAtActionResult> CreateQuiz([FromBody] QuizDTO quizDto, CancellationToken cancellationToken)
         {
+            _logSender.SendLog("Creating a new quiz");
             var createdQuiz = await _quizService.CreateAsync(quizDto, cancellationToken);
-            return CreatedAtAction(nameof(GetQuiz), new { id = createdQuiz.Id }, createdQuiz);
+            _logSender.SendLog($"Created quiz with id {createdQuiz.Id}");
+            return CreatedAtAction("GetQuiz", new { id = createdQuiz.Id }, createdQuiz); // Используем строку "GetQuiz"
         }
 
         [HttpPut("{id}")]
         public async Task<NoContentResult> UpdateQuiz(int id, [FromBody] QuizDTO quizDto, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Updating quiz with id {id}");
             await _quizService.UpdateAsync(id, quizDto, cancellationToken);
+            _logSender.SendLog($"Updated quiz with id {id}");
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<NoContentResult> DeleteQuiz(int id, CancellationToken cancellationToken)
         {
+            _logSender.SendLog($"Deleting quiz with id {id}");
             await _quizService.DeleteAsync(id, cancellationToken);
+            _logSender.SendLog($"Deleted quiz with id {id}");
             return NoContent();
         }
     }
