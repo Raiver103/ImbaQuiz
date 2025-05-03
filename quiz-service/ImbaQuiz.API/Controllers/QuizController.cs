f4ae209943dcd10ed21c9ec6e4435db8d888e363
raiver <ilya.maksimovich.01@gmail.com>
2025-05-03 14:57:17 +0300

    add swagger annotation

--- quiz-service/ImbaQuiz.API/Controllers/QuizController.cs
+++ quiz-service/ImbaQuiz.API/Controllers/QuizController.cs
@@ -1,48 +1,76 @@
-﻿    using ImbaQuiz.Application.DTOs;
-    using ImbaQuiz.Application.Interfaces; 
-    using ImbaQuiz.Domain.Exceptions;
-    using ImbaQuiz.Domain.Interfaces;
-    using Microsoft.AspNetCore.Mvc; 
+﻿using ImbaQuiz.Application.DTOs;
+using ImbaQuiz.Application.Interfaces;
+using ImbaQuiz.Domain.Exceptions;
+using ImbaQuiz.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-    namespace ImbaQuiz.API.Controllers
-    { 
-        [ApiController]
-        [Route("api/quizzes")]
-        public class QuizzesController(IQuizService _quizService, ILogSender _logSender) : ControllerBase
+namespace ImbaQuiz.API.Controllers
+{
+    [ApiController]
+    [Route("api/quizzes")]
+    public class QuizzesController(IQuizService _quizService, ILogSender _logSender) : ControllerBase
+    {
+        /// <summary>
+        /// Получить все викторины.
+        /// </summary>
+        /// <param name="cancellationToken">Токен отмены запроса.</param>
+        /// <returns>Список всех викторин.</returns>
+        [HttpGet]
+        public async Task<OkObjectResult> GetAllQuizzes(CancellationToken cancellationToken)
         {
-            [HttpGet]
-            public async Task<OkObjectResult> GetAllQuizzes(CancellationToken cancellationToken)
-            { 
-                var quizzes = await _quizService.GetAllAsync(cancellationToken); 
-                return Ok(quizzes);
-            }
+            var quizzes = await _quizService.GetAllAsync(cancellationToken);
+            return Ok(quizzes);
+        }
 
-            [HttpGet("{id}")]
-            public async Task<OkObjectResult> GetQuiz(int id, CancellationToken cancellationToken)
-            { 
-                var quiz = await _quizService.GetByIdAsync(id, cancellationToken); 
-                return Ok(quiz);
-            }
+        /// <summary>
+        /// Получить викторину по её идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор викторины.</param>
+        /// <param name="cancellationToken">Токен отмены запроса.</param>
+        /// <returns>Объект викторины.</returns>
+        [HttpGet("{id}")]
+        public async Task<OkObjectResult> GetQuiz(int id, CancellationToken cancellationToken)
+        {
+            var quiz = await _quizService.GetByIdAsync(id, cancellationToken);
+            return Ok(quiz);
+        }
 
-            [HttpPost]
-            public async Task<CreatedAtActionResult> CreateQuiz([FromBody] QuizDTO quizDto, CancellationToken cancellationToken)
-            { 
-                var createdQuiz = await _quizService.CreateAsync(quizDto, cancellationToken); 
-                return CreatedAtAction(nameof(GetQuiz), new { id = createdQuiz.Id, cancellationToken = cancellationToken }, createdQuiz); 
-            }
+        /// <summary>
+        /// Создать новую викторину.
+        /// </summary>
+        /// <param name="quizDto">Данные викторины.</param>
+        /// <param name="cancellationToken">Токен отмены запроса.</param>
+        /// <returns>Созданная викторина.</returns>
+        [HttpPost]
+        public async Task<CreatedAtActionResult> CreateQuiz([FromBody] QuizDTO quizDto, CancellationToken cancellationToken)
+        {
+            var createdQuiz = await _quizService.CreateAsync(quizDto, cancellationToken);
+            return CreatedAtAction(nameof(GetQuiz), new { id = createdQuiz.Id }, createdQuiz);
+        }
 
-            [HttpPut("{id}")]
-            public async Task<NoContentResult> UpdateQuiz(int id, [FromBody] QuizDTO quizDto, CancellationToken cancellationToken)
-            { 
-                await _quizService.UpdateAsync(id, quizDto, cancellationToken); 
-                return NoContent();
-            }
+        /// <summary>
+        /// Обновить существующую викторину.
+        /// </summary>
+        /// <param name="id">Идентификатор викторины.</param>
+        /// <param name="quizDto">Обновлённые данные викторины.</param>
+        /// <param name="cancellationToken">Токен отмены запроса.</param>
+        [HttpPut("{id}")]
+        public async Task<NoContentResult> UpdateQuiz(int id, [FromBody] QuizDTO quizDto, CancellationToken cancellationToken)
+        {
+            await _quizService.UpdateAsync(id, quizDto, cancellationToken);
+            return NoContent();
+        }
 
-            [HttpDelete("{id}")]
-            public async Task<NoContentResult> DeleteQuiz(int id, CancellationToken cancellationToken)
-            { 
-                await _quizService.DeleteAsync(id, cancellationToken); 
-                return NoContent();
-            }
+        /// <summary>
+        /// Удалить викторину по идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор викторины.</param>
+        /// <param name="cancellationToken">Токен отмены запроса.</param>
+        [HttpDelete("{id}")]
+        public async Task<NoContentResult> DeleteQuiz(int id, CancellationToken cancellationToken)
+        {
+            await _quizService.DeleteAsync(id, cancellationToken);
+            return NoContent();
         }
     }
+}
